cf1249dda1589e83a0246547cb7935ccef79a573
Jean Carlo <jeansanto2002@gmail.com>
2023-07-25 16:11:25 -0300

    alterando o valorTotal

--- assets/js/metodoFilter.js
+++ assets/js/metodoFilter.js
@@ -20,10 +20,10 @@ function filtrarPorDisponibilidade() {
     return livros.filter(livro => livro.quantidade > 0)
 }
 
-function exibirValorTotalDosLivrosNaPagina() {
+function exibirValorTotalDosLivrosNaPagina(valorTotal) {
     elementoValorTotalDosLivrosDisponiveis.innerHTML = `
     <div class="livros__disponiveis">
-    <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+    <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
   </div>
     `
 }
--- assets/js/metodoReduce.js
+++ assets/js/metodoReduce.js
@@ -1,3 +1,3 @@
-function calcularValorTotalDeLivrosDisponiveis(livros){
+function calcularValorTotalDeLivrosDisponiveis(livros) {
     return livros.reduce((acc, livro) => acc + livro.preco, 0).toFixed(2)
 }
